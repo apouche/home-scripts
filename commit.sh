763f7925bcd4a35b93df2e17f04bfdb2b714cb9a
Johan Attali <johan@smartnsoft.com>
2011-10-14 15:05:13 +0200

    modifications made

--- commit.sh
+++ commit.sh
@@ -5,10 +5,11 @@ SCRIPT_LOGFILE="${SCRIPT_PATH}/git.log"
 cd ${SCRIPT_PATH}
 res=`git status|grep "nothing to commit"`
 if [[ "$res" == "" ]]; then
-	echo "---------------------------------------"  2>&1 >>${SCRIPT_LOGFILE}
-	echo "-- `date '+%y%m%d.%H%M%S'`"  2>&1 >>${SCRIPT_LOGFILE}
-	git add -A 2>&1 >>${SCRIPT_LOGFILE}
-	git commit -a -m "modifications made" 2>&1 >>${SCRIPT_LOGFILE}
+	echo "---------------------------------------"  >>${SCRIPT_LOGFILE} 2>&1
+	echo "-- `date '+%y%m%d.%H%M%S'`"  >>${SCRIPT_LOGFILE} 2>&1
+	git add -A >>${SCRIPT_LOGFILE} 2>&1
+	git commit -a -m "modifications made" >>${SCRIPT_LOGFILE} 2>&1
 	git push 2>&1 >>${SCRIPT_LOGFILE}
-	echo "---------------------------------------\n"  2>&1 >>${SCRIPT_LOGFILE}
+	echo "---------------------------------------"   >>${SCRIPT_LOGFILE} 2>&1
+	echo ""
 fi
