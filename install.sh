1ad0c41816304c5d963f237f279c458c567e79a1
Johan Attali <johan@smartnsoft.com>
2011-10-14 15:39:02 +0200

    added the copieable files into a specfic folder

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #! /bin/bash
 SCRIPT_PATH="$PWD/`dirname $0`"
-SCRIPT_FILES=`find ${SCRIPT_PATH} ! \( -regex ".*/\.git.*" \) -type f |grep -oP "[^/]+$"|tr '\n' ' '`
+GLOBAL_FILES_NAME="files"
+GLOBAL_FILES=`find ${SCRIPT_PATH}/${GLOBAL_FILES_NAME} -type f|grep -oP "[^/]+$"|tr '\n' ' '`
+#GLOBAL_FILES=`find ${SCRIPT_PATH} ! \( -regex ".*/\.git.*" \) -type f |grep -oP "[^/]+$"|tr '\n' ' '`
 
-for file in ${SCRIPT_FILES}
+for file in ${GLOBAL_FILES}
 do
 	res=`find  ~ -name "${file}" -maxdepth 1`
 	if [[ "$res" != "" ]]; then
 		echo "backing up $res into ${res}.mine"
 		mv "$res" "${res}.mine"
 	fi
-	ln -s "${SCRIPT_PATH}/$file" ~/$file
+	ln -s "${SCRIPT_PATH}/${GLOBAL_FILES_NAME}/$file" ~/$file
 done
