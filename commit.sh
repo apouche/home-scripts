b56e10f95b8a679ee8032386861d106dec637e2a
Johan Attali <johan@smartnsoft.com>
2011-10-14 14:57:23 +0200

    modifications made

--- commit.sh
+++ commit.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 SCRIPT_PATH="$PWD/`dirname $0`"
+SCRIPT_LOGFILE="${SCRIPT_PATH}/git.log"
 
 cd ${SCRIPT_PATH}
 res=`git status|grep "nothing to commit"`
 if [[ "$res" == "" ]]; then
-	git add -A
-	git commit -a -m "modifications made"
-	git push
+	git add -A 2>&1 >>${SCRIPT_LOGFILE}
+	git commit -a -m "modifications made" 2>&1 >>${SCRIPT_LOGFILE}
+	git push 2>&1 >>${SCRIPT_LOGFILE}
 fi
