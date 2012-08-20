55c96ed145da729566c9d685b67688fab7d78475
Johan Attali <johan.attali@gmail.com>
2012-08-20 09:58:21 +0200

    modified install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 SCRIPT_PATH="$PWD/`dirname $0`"
 GLOBAL_FILES_NAME="files"
-GLOBAL_FILES=`find ${SCRIPT_PATH}/${GLOBAL_FILES_NAME} -type f|grep -oP "[^/]+$"|tr '\n' ' '`
+GLOBAL_FILES=`find ${SCRIPT_PATH}/${GLOBAL_FILES_NAME} -type f|grep -oE "[^/]+$"|tr '\n' ' '`
 #GLOBAL_FILES=`find ${SCRIPT_PATH} ! \( -regex ".*/\.git.*" \) -type f |grep -oP "[^/]+$"|tr '\n' ' '`
 
+echo ${SCRIPT_PATH}
 for file in ${GLOBAL_FILES}
 do
 	res=`find  ~ -name "${file}" -maxdepth 1`
