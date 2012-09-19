4b730fe58a7f3e13ac2bf2cc538b16bd75ab456b
apouche <johan.attali@gmail.com>
2012-09-19 15:39:04 +0200

    adding bin folder handling to install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
-#! /bin/bash
+#! /bin/zsh
+
 SCRIPT_PATH="$PWD/`dirname $0`"
 GLOBAL_FILES_NAME="files"
 GLOBAL_FOLDERS_NAME="folders"
+GLOBAL_BIN_FOLDER="bin"
 
 GLOBAL_FILES=`find ${SCRIPT_PATH}/${GLOBAL_FILES_NAME} -type f|grep -oE "[^/]+$"|tr '\n' ' '`
 GLOBAL_FOLDERS=`find ${SCRIPT_PATH}/${GLOBAL_FOLDERS_NAME} -type d -maxdepth 1 -mindepth 1|grep -oE "[^/]+$"|tr '\n' ' '`
 
 ALL_FILES="${GLOBAL_FILES} ${GLOBAL_FOLDERS}"
+BIN_FILES=`echo ${SCRIPT_PATH}/${GLOBAL_BIN_FOLDER}/*`
+
+#########################################
+## Creating Links
+#########################################
 
 for file in ${ALL_FILES}
 do
@@ -25,4 +32,10 @@ do
   
 	ln -sf "${SCRIPT_PATH}/${folder}/$file" ~/$file
 done
+
+#########################################
+## Copying to /usr/local/bin
+#########################################
+
+cp ${BIN_FILES} /usr/local/bin/
 
